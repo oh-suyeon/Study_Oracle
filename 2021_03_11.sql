fc376070f2dfba5a32b3f35c571fb9c1acf3fc0c
DESKTOP-IUJNLEG\PC-08 <djs02061@naver.com>
2021-03-11 19:10:35 +0900

    복습

--- 2021_03_11.sql
+++ 2021_03_11.sql
@@ -1,15 +1,18 @@
-select *
-from emp;
-
-SELECT *
-FROM emp;
-
 -- 데이터 조회 방법
 -- FROM : 데이터 조회할 테이블 명시
 -- SELECT : 조회하고자 하는 컬럼명(단, 테이블에 있는 컬럼명만 가능)
 --          테이블의 모든 컬럼을 조회할 경우 *(아스테리스크)를 기술
-SELECT empno, ename  
-FROM EMP;
+
+SELECT * 
+FROM emp;
+
 --EMPNO : 직원번호, ENAME : 직원이름, JOB : 담당업무
 --MGR : 상위 담당자, HIREDATE : 입자일자, SAL : 급여
 --COMM : 상여금, DEPTNO : 부서번호
+
+SELECT empno, ename  
+FROM emp;
+
+SELECT job, sal
+FROM emp;
+
